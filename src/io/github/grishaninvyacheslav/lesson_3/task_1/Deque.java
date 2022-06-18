620574fda8a0435bdce1b6d2237ac9dc7ab5f887
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2022-06-18 20:16:05 +0300

    Вынес LinkedList в utils

--- src/io/github/grishaninvyacheslav/lesson_3/task_1/Deque.java
+++ src/io/github/grishaninvyacheslav/lesson_3/task_1/Deque.java
@@ -1,5 +1,7 @@
 package io.github.grishaninvyacheslav.lesson_3.task_1;
 
+import io.github.grishaninvyacheslav.utils.LinkedList;
+
 class Demo {
     public static void main(String[] args) {
         Deque<Integer> deque = new Deque<>();
@@ -56,83 +58,5 @@ public class Deque<E> {
 
     public boolean isNotEmpty() {
         return !isEmpty();
-    }
-}
-
-class LinkedList<E> {
-    private Node head = null;
-    private Node tail = null;
-
-    public void pushLast(E value) {
-        if (isEmpty()) {
-            tail = new Node(value, null, null);
-            head = tail;
-            return;
-        }
-        tail.next = new Node(value, tail, null);
-        ;
-        tail = tail.next;
-    }
-
-    public E pickLast() {
-        return tail.value;
-    }
-
-    public E popLast() {
-        if (isEmpty()) {
-            throw new RuntimeException();
-        }
-        E lastValue = tail.value;
-        tail = tail.prev;
-        if (tail != null) {
-            tail.next = null;
-        } else {
-            head = null;
-        }
-        return lastValue;
-    }
-
-    public void pushFirst(E value) {
-        if (isEmpty()) {
-            tail = new Node(value, null, null);
-            head = tail;
-            return;
-        }
-        head.prev = new Node(value, null, head);
-        head = head.prev;
-    }
-
-    public E pickFirst() {
-        return head.value;
-    }
-
-    public E popFirst() {
-        if (isEmpty()) {
-            throw new RuntimeException();
-        }
-        E firstValue = head.value;
-        head = head.next;
-        if (head != null) {
-            head.prev = null;
-        } else {
-            tail = null;
-        }
-        return firstValue;
-    }
-
-    public boolean isEmpty() {
-        return tail == null;
-    }
-
-    private class Node {
-        public E value;
-        public Node prev;
-        public Node next;
-
-        public Node(E value, Node prev, Node next) {
-            this.value = value;
-            this.prev = prev;
-            this.next = next;
-        }
     }
 }
