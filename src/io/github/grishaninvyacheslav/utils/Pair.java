2cc6ef00b10b3a71596bca6cda9b3b162e46eac2
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2022-06-18 21:29:12 +0300

    Решил задание 3

---
+++ src/io/github/grishaninvyacheslav/utils/Pair.java
@@ -0,0 +1,11 @@
+package io.github.grishaninvyacheslav.utils;
+
+public class Pair<A, B> {
+    public A first;
+    public B second;
+
+    public Pair(A first, B second){
+        this.first = first;
+        this.second = second;
+    }
+}
